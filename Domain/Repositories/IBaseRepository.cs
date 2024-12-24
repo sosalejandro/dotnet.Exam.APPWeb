d574b519fd0a597f428495534b43403aefbe9908
Alejandro Sosa <alejandrososadev@gmail.com>
2024-12-24 16:44:53 -0400

    Adds initial domain repositories

---
+++ Domain/Repositories/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using System.Linq.Expressions;
+
+namespace Domain.Repositories;
+
+public interface IBaseRepository<TEntity> where TEntity : BaseEntity
+{
+    public IQueryable<TEntity> FindAll(bool trackChanges);
+    public IQueryable<TEntity> FindByCondition(
+        Expression<Func<TEntity, bool>> expression,
+        bool trackChanges);
+
+    public void Create(TEntity entity);
+    public void Update(TEntity entity);
+    public void Delete(TEntity entity);
+}
---
+++ Domain/Repositories/IRepositoryManager.cs
@@ -0,0 +1,8 @@
+﻿namespace dotnet.Exam.APPWeb.Domain.Repositories;
+
+public interface IRepositoryManager
+{
+    IBookRepository BookRepository { get; }
+
+    IUnitOfWork UnitOfWork { get; }
+}
