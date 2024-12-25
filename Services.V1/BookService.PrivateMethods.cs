e7e4de16d727297107a615a7b8902046354fef1b
Alejandro Sosa <alejandrososadev@gmail.com>
2024-12-25 00:37:56 -0400

    Adds service layer

---
+++ Services.V1/BookService.PrivateMethods.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Exceptions;
+using dotnet.Exam.APPWeb.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.V1;
+
+internal sealed partial class BookService
+{
+    private async Task<Book> GetBookById(Guid id, bool trackChanges, CancellationToken stoppingToken = default)
+    {
+        try
+        {
+            Book book = await _repositoryManager
+            .BookRepository
+            .GetBookByIdAsync(id, trackChanges, stoppingToken);
+
+            return book;
+        }
+        catch (BookNotFoundException)
+        {
+            throw;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("An error occurred while getting the book", ex);
+        }
+    }
+}
---
+++ Services.V1/ServiceManager.cs
@@ -0,0 +1,21 @@
+﻿using dotnet.Exam.APPWeb.Domain.Repositories;
+using Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.V1;
+
+public class ServiceManager : IServiceManager
+{
+    private readonly Lazy<IBookService> _lazyBookService;
+    public ServiceManager(IRepositoryManager repositoryManager)
+    {
+        _lazyBookService = new(
+            () => new BookService(repositoryManager) ?? throw new ArgumentNullException(nameof(repositoryManager))
+            );
+    }
+    public IBookService BookService => _lazyBookService.Value;
+}
