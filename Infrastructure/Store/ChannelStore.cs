2ca2c765379aa691a087584e805461da457cbeef
Alejandro Sosa <alejandrososadev@gmail.com>
2024-12-25 03:15:07 -0400

    Adds channel to keep track of entities created and detaches them from context on save async calls.
    Includes update to ObservableObject onto the ViewModels
    
    Updates service and unit of work contract and implementation
    
    Adds ChannelStore class

---
+++ Infrastructure/Store/ChannelStore.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Threading.Channels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace Infrastructure.Store;
+public class ChannelStore<TEntity> where TEntity : BaseEntity
+{
+    private Channel<TEntity> _channel;
+
+    public ChannelStore()
+    {
+        _channel = Channel.CreateUnbounded<TEntity>();
+    }
+
+    public async Task AddEntityAsync(TEntity entity)
+    {
+        await _channel.Writer.WriteAsync(entity);
+    }
+
+    public IAsyncEnumerable<TEntity> ReadAllAsync()
+    {
+        return _channel.Reader.ReadAllAsync();
+    }
+
+    public ValueTask<(bool Success, TEntity Entity)> TryReadAsync()
+    {
+        if (_channel.Reader.TryRead(out var entity))
+        {
+            return new ValueTask<(bool, TEntity)>((true, entity));
+        }
+        return new ValueTask<(bool, TEntity)>((false, null));
+    }
+
+    public void Complete()
+    {
+        _channel.Writer.Complete();
+    }
+
+    public void Reset()
+    {
+        _channel = Channel.CreateUnbounded<TEntity>();
+    }
+}
+
